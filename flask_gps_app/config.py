4f1b3a5d3ea6870cfac3df35280bb39686a0adb0
Tanmaya-2006sah <tanmayasah23@gmail.com>
2026-03-16 16:37:34 +0530

    Add Flask configuration settings in config.py

---
+++ flask_gps_app/config.py
@@ -0,0 +1,8 @@
+# Flask Configuration
+
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    DEBUG = True

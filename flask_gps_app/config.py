9ede406654aa8489604e76ee07b558425b2a9c86
Tanmaya-2006sah <tanmayasah23@gmail.com>
2026-04-16 15:01:59 +0530

    Delete flask_gps_app directory

--- flask_gps_app/config.py
+++
@@ -1,8 +0,0 @@
-# Flask Configuration
-
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    DEBUG = True
